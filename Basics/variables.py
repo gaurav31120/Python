5954ee8e0dc07ba9b75920e18ea6269c0aceb16e
Gaurav Kumar <gaurav31120@gmail.com>
2025-07-26 21:48:09 +0530

    4 Variables

---
+++ Basics/variables.py
@@ -0,0 +1,9 @@
+name = "Gaurav Kumar" #name is a string variable
+age = 12              #age is a integer variable
+salary = 23242.32     #salary is a float variable    
+isMarried = False    #isMarried is a boolean variable
+
+print(name)     #Gaurav Kumar
+print(age)      #12
+print(salary)   #23242.32
+print(isMarried) #False
