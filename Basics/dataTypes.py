638909f0e816a0d4fe3e8b93809875fab4024e58
Gaurav Kumar <gaurav31120@gmail.com>
2025-07-26 22:07:33 +0530

    6 Data Types

---
+++ Basics/dataTypes.py
@@ -0,0 +1,20 @@
+a = 12      
+print(type(a))    #integer
+
+b = 56.4
+print(type(b))    #float
+
+c = 12/3     # fration values are also float
+print(type(c))    #float
+
+d = 34j
+print(type(d))    #complex
+
+e = '134225252 gvuyvu iu%^'
+print(type(e))    #string
+
+f = "gidfgtw868w7g8 8 woiewbuoif ufbe87*&^%&Tt7"
+print(type(f))    #string
+
+g = True
+print(type(g))    #boolean
